bcc6dced69e2fcf56b92365c3706f793ba6238cc
Daniel Oliveira <daniel.santos@venturus.org.br>
2024-11-04 21:31:44 -0300

    Setting up the project

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        My first next + tailwind page
-      </h1>
-    </div>
-  );
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#F71D77",
+        },
+        "dark-blue": "#091529",
+        "light-blue": "#182947",
+      },
+    },
   },
   plugins: [],
 };
